518e22a395f272f8ce5c71e7c1f520fa54f75aeb
dynatenshi <lomonosov.fedechka@gmail.com>
2026-01-17 17:28:24 +0300

    added tests

--- UserService/src/test/java/ru/darkslayer/userservice/UserServiceApplicationTests.java
+++ UserService/src/test/java/ru/darkslayer/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package ru.darkslayer.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.darkslayer.userservice.entity.User;
+import ru.darkslayer.userservice.repository.UserRepository;
 
+@Slf4j
 @SpringBootTest
 class UserServiceApplicationTests {
+
+    public static final int NUMBER_OF_USERS = 100;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void createUsers() {
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            User user = User.builder()
+                    .name("User " + i)
+                    .surname("resU " + i)
+                    .email("email" + i + "@gmail.com")
+                    .address("Address " + i)
+                    .alerting(i % 3 == 0)
+                    .energyAlertingThreshold(1000 * i%10)
+                    .build();
+        }
+        log.info("User repository updated");
+    }
 }
